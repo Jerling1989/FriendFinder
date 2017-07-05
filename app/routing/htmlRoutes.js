37cbf499c88079e5bdb4e6dfc0ef191e3909277f
Jacob Erling <jacobs_macbook@Jacobs-MacBook-Pro.local>
2017-07-05 18:22:04 -0400

    server setup

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,22 @@
+var path = require("path");
+
+
+var fileRoutes = function(app) {
+
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+
+// Might not be necessary
+    app.get("/home", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+
+    // If no matching route is found default to home
+    app.use(function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+};
+
+module.exports = fileRoutes;
