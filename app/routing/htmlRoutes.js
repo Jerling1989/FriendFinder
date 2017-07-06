903fb45c43d07fe7315ff9ddae6fc30f6bca79f8
Jacob Erling <jacobs_macbook@Jacobs-MacBook-Pro.local>
2017-07-06 11:37:49 -0400

    css and image file paths

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,13 +2,25 @@ var path = require("path");
 
 var fileRoutes = function(app) {
 
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
-// Might not be necessary
-    app.get("/home", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    // CSS & Image Paths
+    app.get("/reset", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/assets/css/reset.css"));
+    });
+
+    app.get("/style", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/assets/css/style.css"));
+    });
+
+    app.get("/img", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/assets/img/friend-bg.jpg"));
     });
 
     // If no matching route is found default to home
